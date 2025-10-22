f8893506f67d6f58f848795f3fa05d705b1e76ad
Mauricio-100 <ceoseshell@gmail.com>
2025-10-22 12:26:24 +0100

    Create deploy.py

---
+++ modules/deploy.py
@@ -0,0 +1,4 @@
+def run(args):
+    print("🚀 Déploiement GopuOS (simulé)")
+    print("Cette commande peut être connectée à Render, DigitalOcean, etc.")
+    print("Exemple: gopu deploy --provider render --image ubuntu-24")
