f4c8ddc51853f8d459bc5d5d087bf04ea957e6ec
Mauricio-100 <ceoseshell@gmail.com>
2025-10-23 14:12:01 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     entry_points={
         "console_scripts": [
-            "gopu = gopu.Gopu:main"
+            "gopu = gopu.gopu:main"
         ]
     },
     install_requires=[
